95bdfc29c5ce36e0508769864e68ec869026449b
aidanjohnys <aidan.johnys@gmail.com>
2022-11-17 16:27:15 +0000

    epic changes seperate js file

--- TEXT.js
+++ TEXT.js
@@ -1,3 +1,20 @@
+const all_Titles = [
+    "Gameification",
+    "Emerging Technology",
+    "Whistle Blowing"
+];
+
+const all_Overviews = [
+    "",
+    "",
+    "Whistle blowing a way of bringing forth information about a company that is in the public interest.",
+    ]
 
 
-document.getElementById("title").innerHTML = 'GAEN';
+let instance = 2;
+let title = all_Titles[instance];
+let overview = all_Overviews[instance]
+
+
+document.getElementById("title").innerHTML = title;
+document.getElementById("overview").innerHTML = title;
