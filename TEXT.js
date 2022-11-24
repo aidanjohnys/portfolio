f687804b7678bd509edb3ce1537a401f88464b1d
aidan <aidan@DESKTOP-4SLRJLF>
2022-11-24 10:15:51 +0000

    updated javascript to allow changing paragraph text easier

--- TEXT.js
+++ TEXT.js
@@ -1,20 +1,53 @@
 const all_Titles = [
     "Gameification",
     "Emerging Technology",
-    "Whistle Blowing"
+    "Whistle Blowing",
 ];
 
 const all_Overviews = [
     "",
     "",
-    "Whistle blowing a way of bringing forth information about a company that is in the public interest.",
-    ]
+    "Whistle blowing is when an employee working at an organisation exposes their bad practises, particually when that information is in the public intrest, such as a company hurting the health of their customers or defrauding consumers.",
+]
 
+const all_Header1 = [
+    "",
+    "",
+    "Why blow the whistle?",
+]
 
-let instance = 2;
-let title = all_Titles[instance];
-let overview = all_Overviews[instance]
+const all_Para1 = [
+    "",
+    "",
+    "",
+]
 
+const all_Header2 = [
+    "",
+    "",
+    "Case Studies",
+]
 
-document.getElementById("title").innerHTML = title;
-document.getElementById("overview").innerHTML = title;
+const all_Para2 = [
+    "",
+    "",
+    "<ul><li>" + "Coffee" + "</li><li>" + "Tea" + "</li><li>" + "Milk" + "</li></ul>",
+]
+
+function update_Content(instance) {
+    let title = all_Titles[instance];
+    let overview = all_Overviews[instance];
+    let left_Header = all_Header1[instance];
+    let left_Para = all_Para1[instance];
+    let right_Header = all_Header2[instance];
+    let right_Para = all_Para2[instance];
+
+    document.getElementById("title").innerHTML = title;
+    document.getElementById("overview").innerHTML = overview;
+    document.getElementById("left_Header").innerHTML = left_Header;
+    document.getElementById("left_Para").innerHTML = left_Para;
+    document.getElementById("right_Header").innerHTML = right_Header;
+    document.getElementById("right_Para").innerHTML = right_Para;
+}
+
+update_Content(0);
